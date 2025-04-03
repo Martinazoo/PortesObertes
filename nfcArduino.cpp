48f59890643872c8c590a6288fb83cbf10edb886
Martinazoo <paumartinnadal@gmail.com>
2025-04-03 15:48:13 +0200

    Arduino amb led

--- nfcArduino.cpp
+++ nfcArduino.cpp
@@ -3,23 +3,30 @@
 
 #define RST_PIN	9    
 #define SS_PIN	10   
+#define LED_PIN 8
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
 	Serial.begin(9600); 
 	SPI.begin();        
+  pinMode(LED_PIN, OUTPUT);
 	mfrc522.PCD_Init(); 
 }
 
 void loop() {
 	if ( mfrc522.PICC_IsNewCardPresent()){  
-        if ( mfrc522.PICC_ReadCardSerial()) {
-            for (byte i = 0; i < mfrc522.uid.size; i++) {
-                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-                Serial.print(mfrc522.uid.uidByte[i], HEX);   
-            } 
-            Serial.println();
-            mfrc522.PICC_HaltA();         
-        }      
+    if ( mfrc522.PICC_ReadCardSerial()) {
+      digitalWrite(LED_PIN, HIGH);
+          for (byte i = 0; i < mfrc522.uid.size; i++) {
+            Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+            Serial.print(mfrc522.uid.uidByte[i], HEX);   
+          } 
+          Serial.println();
+          mfrc522.PICC_HaltA();  
+          delay(1000);       
+    }      
 	}	
+  
+  digitalWrite(LED_PIN, LOW);
 }
