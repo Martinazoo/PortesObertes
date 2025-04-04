8affa319d4f3eb0b1db0694dc4ec59e6241705b1
Martinazoo <paumartinnadal@gmail.com>
2025-04-04 14:44:04 +0200

    asd

--- nfcArduino.cpp
+++ nfcArduino.cpp
@@ -8,6 +8,30 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+// Notas (frecuencias) y duraciones de la intro de Super Mario Bros (versión adaptada)
+int melody[] = {
+  660, 660, 0, 660, 0, 520, 660, 0, 730
+};
+
+
+int noteDurations[] = {
+  100, 100, 100, 100, 100, 100, 100, 100, 100
+};
+
+void playMarioIntro() {
+  for (int i = 0; i < sizeof(melody) / sizeof(int); i++) {
+    int noteDuration = noteDurations[i];
+    if (melody[i] == 0) {
+      delay(noteDuration);
+    } else {
+      tone(BUZZER, melody[i], noteDuration);
+      delay(noteDuration);
+      noTone(BUZZER);
+    }
+    delay(30); // pequeña pausa entre notas
+  }
+}
+
 void setup() {
   Serial.begin(9600); 
   SPI.begin();        
@@ -19,28 +43,25 @@ void setup() {
 void loop() {
   if (mfrc522.PICC_IsNewCardPresent()) {  
     if (mfrc522.PICC_ReadCardSerial()) {
-      //Serial print para connectarme con la raspi 
       digitalWrite(LED_PIN, HIGH);
+      
       for (byte i = 0; i < mfrc522.uid.size; i++) {
         Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
         Serial.print(mfrc522.uid.uidByte[i], HEX);   
       } 
       Serial.println();
       mfrc522.PICC_HaltA();  
-
-      int melody[] = {659, 622, 659, 622, 659, 494, 587, 523, 440, 440, 494, 523}; 
-      int durations[] = {150, 150, 150, 150, 150, 150, 150, 150, 250, 150, 150, 300}; 
-
-      for (int i = 0; i < 12; i++) {
-        tone(BUZZER, melody[i], durations[i]);
-        delay(durations[i] * 1.3);
-        noTone(BUZZER);
+      if (Serial.available()){
+        String response = Serial.readStringUntil('\n');
+        Serial.print("Respuesta de la Raspi: ");
+        Serial.println(response);
       }
+    }
+      // Aquí suena la intro de Mario Bros
+      playMarioIntro();
 
       delay(1000); 
-    }      
-  }	
-  
+  }      
   digitalWrite(LED_PIN, LOW);
-}
-//Funcion lectura de NFCID i otra para obtener la respuesta 
+}	
+  
